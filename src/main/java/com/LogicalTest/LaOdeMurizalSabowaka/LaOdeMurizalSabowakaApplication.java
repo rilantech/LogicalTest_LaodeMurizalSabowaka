7a264b167d802d095c15e450fbf428909c8c7316
rilantech <laodemurizal7@gmail.com>
2025-10-17 16:28:12 +0700

    setup project

---
+++ src/main/java/com/LogicalTest/LaOdeMurizalSabowaka/LaOdeMurizalSabowakaApplication.java
@@ -0,0 +1,13 @@
+package com.LogicalTest.LaOdeMurizalSabowaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaOdeMurizalSabowakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaOdeMurizalSabowakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LogicalTest/LaOdeMurizalSabowaka/LaOdeMurizalSabowakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LogicalTest.LaOdeMurizalSabowaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaOdeMurizalSabowakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
